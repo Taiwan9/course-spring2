61c8c27932ef721cefe6a3c79bea592391f3ea19
Taiwan9 <clownser1994@gmail.com>
2022-02-06 19:44:56 -0300

    Exception handling - delete

---
+++ src/main/java/com/clownser/course/servicies/exceptions/DataBaseException.java
@@ -0,0 +1,11 @@
+package com.clownser.course.servicies.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
