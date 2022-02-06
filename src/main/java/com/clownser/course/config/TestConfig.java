387063839cc851066a3e46f93e957f437172e181
Taiwan9 <clownser1994@gmail.com>
2022-02-06 17:02:38 -0300

    Payment, one-to-one association

--- src/main/java/com/clownser/course/config/TestConfig.java
+++ src/main/java/com/clownser/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.clownser.course.entities.Category;
 import com.clownser.course.entities.Order;
 import com.clownser.course.entities.OrderItem;
+import com.clownser.course.entities.Payment;
 import com.clownser.course.entities.Product;
 import com.clownser.course.entities.User;
 import com.clownser.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepositosy.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepositosy.save(o1);
 		
 		
 
